9c09efb04fee420e8a7a975ce3e35fa5930c5813
mhorowitz9 <mhorowitz9@fordham.edu>
2024-11-04 09:48:42 -0500

    Added collision detection to prevent car from falling off at game start

--- lab3/main_game/area_2d.gd
+++ lab3/main_game/area_2d.gd
@@ -1,15 +1,11 @@
-extends Node2D
+extends Area2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		get_tree().change_scene_to_file("res://main_game/level_1.tscn")
